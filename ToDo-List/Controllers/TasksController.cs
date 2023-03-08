f4acbb033cbc8145aae3a20ac557a3bebeadfc24
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-03-08 09:36:57 +0200

    Add Tasks Controller

---
+++ ToDo-List/Controllers/TasksController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDo_List.Controllers;
+public class TasksController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
